6382d858a7abb646c2ba1d01236b7aad3b63e213
randoe0807 <briotsoita@gmail.com>
2024-01-08 23:06:43 +0900

    practice 11: useState Basic 1

--- frontend/react-basic/src/components/Basic1.js
+++ frontend/react-basic/src/components/Basic1.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Basic1 = (props) => {
     const clickHandler = () => {
         console.log("Button Clicked");
     }
+
+    // practice useState
+    const [count, setCount] = useState(0);
+
   return (
     <>
+        <button onClick={() => setCount(count+1)}>Count {count}</button>
         <h1>from Basic1</h1>
         <p>this component is called from Basic1.js</p>
         <p>props.name: {props.name}</p>
-        <button onClick={clickHandler}>Click Me</button>
+        {/* <button onClick={clickHandler}>Click Me</button> */}
     </>
 
   )

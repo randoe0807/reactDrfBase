61dd6c2d0b888ea710b2ef46d93aaaf34d4db7d3
randoe0807 <briotsoita@gmail.com>
2024-01-08 22:55:01 +0900

    practice 9: eventhandler

--- frontend/react-basic/src/components/Basic1.js
+++ frontend/react-basic/src/components/Basic1.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Basic1 = (props) => {
+    const clickHandler = () => {
+        console.log("Button Clicked");
+    }
   return (
     <>
         <h1>from Basic1</h1>
         <p>this component is called from Basic1.js</p>
         <p>props.name: {props.name}</p>
+        <button onClick={clickHandler}>Click Me</button>
     </>
 
   )

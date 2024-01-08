38206d9ae962ecfa63c39478910789ec99e0536e
randoe0807 <briotsoita@gmail.com>
2024-01-08 22:47:30 +0900

    practice 8: Props

--- frontend/react-basic/src/components/Basic1.js
+++ frontend/react-basic/src/components/Basic1.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Basic1 = () => {
+const Basic1 = (props) => {
   return (
     <>
         <h1>from Basic1</h1>
         <p>this component is called from Basic1.js</p>
+        <p>props.name: {props.name}</p>
     </>
 
   )

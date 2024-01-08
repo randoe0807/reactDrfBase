d30a16d9ff213ffc1ad95fefad9cd74099214601
randoe0807 <briotsoita@gmail.com>
2024-01-08 22:42:55 +0900

    practice 7 : Functional component

---
+++ frontend/react-basic/src/components/Basic1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Basic1 = () => {
+  return (
+    <>
+        <h1>from Basic1</h1>
+        <p>this component is called from Basic1.js</p>
+    </>
+
+  )
+}
+
+export default Basic1
